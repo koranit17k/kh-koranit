685afb2286e1bac4c7581c6cdcff8cd64227fa83
koranit17k <koranit.17k@gmail.com>
2026-05-07 21:15:14 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   extends : "docus",
   devtools: { enabled: false },
   compatibilityDate: "2024-04-03",
